e5e63e97002154bf60f582eacae2b9d5ec3f2cc9
sagar-gupta3 <nbzsagar@gmail.com>
2023-08-16 18:02:57 +0530

    done modification in sum.java &diff.java

---
+++ Diff.java
@@ -0,0 +1,10 @@
+public class Diff{
+    /**
+     * @param args
+     */
+     public static void main(String[] args) {
+        int a=5,b=6; 
+
+        System.out.println("sum is " +(a-b));
+    }
+}
