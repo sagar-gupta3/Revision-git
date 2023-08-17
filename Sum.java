ae7ba37dff148df11af1b896e9807b637bf32e9a
sagar-gupta3 <nbzsagar@gmail.com>
2023-08-17 12:01:15 +0530

    added new comment in Sum.java of quicksort branch

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum{
      * @param args
      */
      public static void main(String[] args) {
-        int a=7,b=6;
+        int a=7,b=6;//quicksort
         System.out.println("sum is"+a+b);
     }
 }
