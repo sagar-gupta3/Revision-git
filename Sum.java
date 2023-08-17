7509bc80038ed33e588d4d65c7582d45cbbc149b
sagar-gupta3 <nbzsagar@gmail.com>
2023-08-17 11:58:52 +0530

    added comment in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum{
      * @param args
      */
      public static void main(String[] args) {
-        int a=7,b=6;
+        int a=7,b=6;//bubble sort
         System.out.println("sum is"+a+b);
     }
 }
