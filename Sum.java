7b3438c9e3fc610620c6395f9f47ebc706cb5d7c
sagar-gupta3 <nbzsagar@gmail.com>
2023-08-17 10:58:23 +0530

    changed value of a from vim editor

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum{
      * @param args
      */
      public static void main(String[] args) {
-        int a=5,b=6;
+        int a=7,b=6;
         System.out.println("sum is"+a+b);
     }
 }
