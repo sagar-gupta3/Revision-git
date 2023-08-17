91788131594103bc976ae41c355d80a28ca655ec
Sagar Gupta <67269344+sagar-gupta3@users.noreply.github.com>
2023-08-17 18:44:31 +0530

    Update Diff.java

--- Diff.java
+++ Diff.java
@@ -3,7 +3,7 @@ public class Diff{
      * @param args
      */
      public static void main(String[] args) {
-        int a=7,b=6; 
+        int a=7,b=6; //checking stash command
 
         System.out.println("sum is " +(a-b));
     }
