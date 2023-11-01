6006b99d043a612cd5142fad0c330800de91f733
sagar-gupta3 <nbzsagar@gmail.com>
2023-11-01 17:52:24 +0530

    created file multiply.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply{
+    /**
+     * @param args
+     */
+     public static void main(String[] args) {
+        int a=5,b=6;
+        System.out.println("sum is"+(a*b));
+
+    
+    }
+}
