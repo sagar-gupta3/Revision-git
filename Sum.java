e75c6f1f42bb77c660f66e38b0887ef941f76bb0
sagar-gupta3 <nbzsagar@gmail.com>
2023-11-01 17:05:37 +0530

    removed merged conflict from stash

--- Sum.java
+++ Sum.java
@@ -4,8 +4,10 @@ public class Sum{
      */
      public static void main(String[] args) {
 
-        int a=7,b=6;//quicksort bubble sort
+        int a,b;
+        a=5,b=6;
+        //quicksort bubble sort
 
-        System.out.println("sum is"+a+b);
+        System.out.println("sum is"+a+b);}}
     }
 }
